f07adfc6209520cf52df778be6ae043256cfd4a2
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-09-03 09:59:58 +0000

    added lineage

---
+++ lineage_bringup.sh
@@ -0,0 +1,29 @@
+echo "Cloning AOSP trees for spes/Redmi note 11"
+git clone https://github.com/crdroidandroid/android_device_xiaomi_spes device/xiaomi/spes
+git clone https://github.com/t25ons/device_xiaomi_sm6225-common.git device/xiaomi/sm6225-common
+git clone https://github.com/crdroidandroid/header_kernel_xiaomi_spes kernel/xiaomi/spes
+git clone https://github.com/crdroidandroid/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+echo "Device Kernel and Vendor Trees in Place now "
+clear
+echo "Quallcomm's dependencies syncing"
+git clone https://github.com/PixelExperience-Devices/device_qcom_common.git device/qcom/common
+git clone https://github.com/PixelExperience-Devices/device_qcom_common-sepolicy.git device/qcom/common-sepolicy
+git clone https://github.com/PixelExperience-Devices/device_qcom_qssi.git device/qcom/qssi
+git clone https://github.com/PixelExperience-Devices/device_qcom_vendor-common.git device/qcom/vendor-common
+git clone https://github.com/PixelExperience-Devices/device_qcom_wlan.git device/qcom/wlan
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_qcom_common.git vendor/qcom/common
+echo "QCOM/COMMON Source dependencies"
+git clone https://github.com/PixelExperience/vendor_qcom_opensource_commonsys-intf_bluetooth.git vendor/qcom/opensource/commonsys-intf/bluetooth
+git clone https://github.com/PixelExperience/vendor_qcom_opensource_core-utils.git vendor/qcom/opensource/core-utils
+git clone https://github.com/PixelExperience/vendor_qcom_opensource_commonsys_dpm.git vendor/qcom/opensource/commonsys/dpm
+echo "Quallcomm source synced"
+clear
+echo "Syncing Device specific dependencies for spes"
+git clone https://github.com/PixelExperience/hardware_qcom-caf_bengal_audio.git hardware/qcom-caf/bengal/audio
+git clone https://github.com/PixelExperience/hardware_qcom-caf_bengal_display.git hardware/qcom-caf/bengal/display
+git clone https://github.com/PixelExperience/hardware_qcom-caf_bengal_gps.git hardware/qcom-caf/bengal/gps
+git clone https://github.com/PixelExperience/hardware_qcom-caf_bengal_media.git hardware/qcom-caf/bengal/media
+echo "Syned Everything"
+
+
