2381371f5bc18575eaa373538e910cd76fea2752
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-10-04 08:07:19 +0530

    Android make file for removing Google bloat

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    Drive \
+    Maps \
+    PrebuiltGmail \
+    YouTube \
+    AndroidAuto \
+    Recorder \
+    PrintServiceGoogle \
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
