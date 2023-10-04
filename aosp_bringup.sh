a497a1c21aa98eb9346b9fa83eca9ef9e12eb8dc
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-10-04 07:49:04 +0530

    revert back to pe/common

--- aosp_bringup.sh
+++ aosp_bringup.sh
@@ -1,8 +1,7 @@
 echo "Cloning AOSP trees for spes/Redmi note 11"
 git clone https://github.com/PixelExperience-Devices/device_xiaomi_spes.git device/xiaomi/spes
-git clone https://github.com/greenforce-project/kernel_xiaomi_spes_sm6225.git kernel/xiaomi/spes-kernel
-git clone https://github.com/PixelExperience-Devices/device_xiaomi_spes-kernel.git device/xiaomi/spes-kernel
-git clone https://github.com/t25ons/device_xiaomi_sm6225-common.git device/xiaomi/sm6225-common
+git clone https://github.com/mi-sdm680/android_kernel_xiaomi_sm6225.git  kernel/xiaomi/sm6225
+git clone https://github.com/PixelExperience-Devices/device_xiaomi_sm6225-common.git device/xiaomi/sm6225-common
 git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_spes.git vendor/xiaomi/spes
 git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6225-common.git vendor/xiaomi/sm6225-common
 git clone https://github.com/PixelExperience/hardware_xiaomi hardware/xiaomi
