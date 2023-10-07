251c0dc727954bf2ae74689ae4160c55890837b4
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-10-07 14:57:52 +0530

    sayanSir's fork and mi680 oss kernel

--- temp.sh
+++ temp.sh
@@ -1,12 +1,8 @@
 echo "Cloning AOSP trees for spes/Redmi note 11"
 git clone https://github.com/sayann70/device_xiaomi_spes.git device/xiaomi/spes
-git clone https://github.com/PixelExperience-Devices/device_xiaomi_spes-kernel.git kernel/xiaomi/spes-kernel
-git clone https://github.com/mi-sdm680/android_kernel_xiaomi_sm6225.git /kernel/xiaomi/spes
 git clone https://github.com/t25ons/device_xiaomi_sm6225-common.git device/xiaomi/sm6225-common
-git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_spes.git vendor/xiaomi/spes
-git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6225-common.git vendor/xiaomi/sm6225-common
 git clone https://github.com/PixelExperience/hardware_xiaomi hardware/xiaomi
-git clone https://github.com/PixelExperience/packages_resources_devicesettings.git packages/resources/devicesettings
+git clone https://github.com/mi-sdm680/android_kernel_xiaomi_sm6225.git kernel/xiaomi/sm6225
 echo "Device Kernel and Vendor Trees in Place now "
 clear
 echo "Syned Everything"
