26466649aea3bccbce945f7b4a775e9316b2dfef
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-10-07 17:35:09 +0530

    added vendor

--- jabiyeff.sh
+++ jabiyeff.sh
@@ -2,7 +2,7 @@ echo "Cloning AOSP trees for spes/Redmi note 11"
 git clone https://github.com/sayann70/device_xiaomi_spes.git device/xiaomi/spes
 git clone https://github.com/PixelExperience/hardware_xiaomi hardware/xiaomi
 git clone https://github.com/mi-sdm680/android_kernel_xiaomi_sm6225.git kernel/xiaomi/sm6225
-
+git clone https://github.com/iamharshit188/vendor_xiaomi_spes /vendor/xiaomi/spes
 echo "Device Kernel and Vendor Trees in Place now "
 clear
 echo "Syned Everything"
