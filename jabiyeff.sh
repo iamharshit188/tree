135e1cc38a525272777c8d763084b36ddb02a2b1
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-12-09 11:38:13 +0000

    Sync only last commits for DeviceTrees

--- jabiyeff.sh
+++ jabiyeff.sh
@@ -1,5 +1,5 @@
 echo "Cloning AOSP trees for spes/Redmi note 11"
-git clone https://github.com/bleedingedgeandroid/device_xiaomi_spes.git device/xiaomi/spes 
+git clone https://github.com/bleedingedgeandroid/device_xiaomi_spes.git device/xiaomi/spes --depth 1
 git clone https://github.com/PixelExperience/hardware_xiaomi hardware/xiaomi --depth 1
 git clone https://github.com/bleedingedgeandroid/kernel_xiaomi_sm6225.git kernel/xiaomi/sm6225 --depth 1
 git clone https://github.com/bleedingedgeandroid/vendor_xiaomi_spes.git /vendor/xiaomi/spes --depth 1 
