5faa0615dc90ad66243f1a8fd088c683f2ce2cb3
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-12-09 11:35:56 +0000

    Added Temporary Trees

--- jabiyeff.sh
+++ jabiyeff.sh
@@ -1,8 +1,8 @@
 echo "Cloning AOSP trees for spes/Redmi note 11"
-git clone https://github.com/iamharshit188/device_xiaomi_spes.git device/xiaomi/spes 
+git clone https://github.com/bleedingedgeandroid/device_xiaomi_spes.git device/xiaomi/spes 
 git clone https://github.com/PixelExperience/hardware_xiaomi hardware/xiaomi --depth 1
-git clone https://github.com/mi-sdm680/android_kernel_xiaomi_sm6225.git kernel/xiaomi/sm6225 --depth 1
-git clone https://github.com/iamharshit188/vendor_xiaomi_spes /vendor/xiaomi/spes --depth 1 
+git clone https://github.com/bleedingedgeandroid/kernel_xiaomi_sm6225.git kernel/xiaomi/sm6225 --depth 1
+git clone https://github.com/bleedingedgeandroid/vendor_xiaomi_spes.git /vendor/xiaomi/spes --depth 1 
 echo "Device Kernel and Vendor Trees in Place now "
 clear
 echo "Syned Everything"
