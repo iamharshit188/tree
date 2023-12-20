b60ee122e4b23a906d43a0c18d6341e5d38fa445
PugzAreCute <me@pugzarecute.com>
2023-12-20 20:13:54 +0530

    Drop dpm
    Already included in skylineui source

--- chris-trees/bringup.sh
+++ chris-trees/bringup.sh
@@ -15,7 +15,6 @@ git clone https://github.com/PixelExperience-Devices/device_qcom_vendor-common.g
 git clone https://github.com/PixelExperience-Devices/device_qcom_wlan.git device/qcom/wlan -b fourteen
 git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_qcom_common.git vendor/qcom/common -b fourteen
 git clone https://github.com/PixelExperience-Staging/vendor_qcom_opensource_commonsys-intf_bluetooth.git vendor/qcom/opensource/commonsys-intf/bluetooth -b fourteen
-git clone https://github.com/PixelExperience-Staging/vendor_qcom_opensource_commonsys_dpm.git vendor/qcom/opensource/commonsys/dpm -b fourteen
 git clone https://github.com/iamharshit188/device_xiaomi_sm6225-common.git device/xiaomi/sm6225-common -b fourteen-skylineui
 git clone https://github.com/PixelExperience-Devices/kernel_xiaomi_sm6225.git kernel/xiaomi/sm6225  -b fourteen
 git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_spes.git vendor/xiaomi/spes  -b fourteen
