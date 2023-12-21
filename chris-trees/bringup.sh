66790d6ebf5331805c41da182f51fd7f3af9c8d5
PugzAreCute <me@pugzarecute.com>
2023-12-21 18:05:51 +0530

    Use ls instead of ~~bash~~ trash

--- chris-trees/bringup.sh
+++ chris-trees/bringup.sh
@@ -33,7 +33,7 @@ git clone https://github.com/AOSPA/android_external_wpa_supplicant_8 external/wp
 
 cd external/wpa_supplicant_8 
 echo -e "${GREEN} Patching WiFi for spes ${CLEAR}"
-for FILE in $SCRIPT_DIR/patches/wpa_supplicant_8/*; do 
+for FILE in $(ls $SCRIPT_DIR/patches/wpa_supplicant_8/*); do 
     echo -e "${GREEN} Applying patch ${FILE} ${CLEAR}"
     git apply $FILE
 done
